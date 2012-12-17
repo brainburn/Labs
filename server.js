14dc293ed3f212d2c372752c3d626ebff6cc97b2
Nikola Chonkov <nikola.chonkov@gmail.com>
2012-12-17 00:46:40 -0500

    test changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http'),
 
 http.createServer(function (request, response){
 	response.writeHead(200, {'Content-Type' : 'text/plain'});
-	response.end('Hello World');
+	response.end('Hello World 2');
 }).listen(port);
 
 console.log('Server is running at http://' + os.hostname() + ":" + port);
