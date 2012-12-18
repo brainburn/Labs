8a2f6b23ed68dc85b8e3f0a8dd3b6bfdf1244f0b
Nikola Chonkov <nikola.chonkov@gmail.com>
2012-12-17 20:46:38 -0500

    stage 1

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var http = require('http'),
 	os = require('os'),
-	port = process.env.PORT || 8000;
+	port = process.env.PORT || 8000,
+	url = require('url'),
+	fs = require('fs');
 
 http.createServer(function (request, response){
-	response.writeHead(200, {'Content-Type' : 'text/plain'});
-	response.end('Hello World 2');
+	var requestUrl = url.parse(request.url, true);
+	var action = requestUrl.pathname;
+	console.log(action);
+	if(action == '/pixel.gif'){
+		console.log('somebody saw the image...');
+		var img = fs.readFileSync('./pixel.gif');
+		response.writeHead(200, {'Content-Type':'image/gif'});
+		response.end(img, 'binary');
+	}else{
+		response.writeHead(200, {'Content-Type' : 'text/plain'});
+		response.end('Hello World 2');
+	}
 }).listen(port);
 
 console.log('Server is running at http://' + os.hostname() + ":" + port);
